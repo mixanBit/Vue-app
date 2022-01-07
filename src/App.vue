eddc968965d227ec61ec7da81524547cf77c5039
mixanBit <65313661+mixanBit@users.noreply.github.com>
2022-01-07 16:11:13 +0300

    Testing vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-example></app-example>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
